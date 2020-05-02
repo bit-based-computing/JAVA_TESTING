67adc5e9bb51ff76611b84fc5a0909a3ed091911
foysalahmmed <foysalahmmed73@gmail.com>
2020-05-02 15:07:34 +0600

    add junit test class for calculator class

--- src/JunitTesting/Calculator.java
+++ src/JunitTesting/Calculator.java
@@ -1,12 +1,14 @@
 package JunitTesting;
 
 public class Calculator {
-    public int add(int a, int b){
+
+    public int add(int a, int b) {
         return a + b;
     }
-    public int add(int[] values){
+
+    public int add(int[] values) {
         int result = 0;
-        for(int value:values){
+        for (int value : values) {
             result += value;
         }
         return result;
