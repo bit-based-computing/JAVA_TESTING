1a26e69411c1215e16e138e0633ad03bfe5e5d60
foysalahmmed <foysalahmmed73@gmail.com>
2020-05-02 15:05:13 +0600

    add junit test class for calculator class

---
+++ src/JunitTesting/Calculator.java
@@ -0,0 +1,14 @@
+package JunitTesting;
+
+public class Calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int add(int[] values){
+        int result = 0;
+        for(int value:values){
+            result += value;
+        }
+        return result;
+    }
+}
---
+++ tests/JunitTesting/CalculatorTest.java
@@ -0,0 +1,25 @@
+package JunitTesting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddIntInt(){
+        Calculator calculator = new Calculator();
+        int a = 6 , b = 10;
+        int actual = calculator.add(a,b);
+        Assert.assertEquals(16,actual);
+    }
+
+    @Test
+    public void testAddIntArray(){
+        int[] intArray = {1,2,3,4,5,6,7,8,9,10};
+        Calculator calculator = new Calculator();
+        int actual = calculator.add(intArray);
+        Assert.assertEquals(55, actual);
+    }
+}
